efd50b9e52f0712c75143442f642f262c58162ba
Khoa Nguyen <khoanguyen@km1.local>
2022-05-18 22:02:37 -0700

    added files

---
+++ src/GameFrame.java
@@ -0,0 +1,21 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class GameFrame extends JFrame {
+
+    GamePanel panel;
+    Image img = Toolkit.getDefaultToolkit().getImage("field.png");
+    GameFrame() {
+        panel = new GamePanel();
+        this.add(panel);
+        this.setTitle("Pong Game");
+        this.setResizable(false);
+        this.setBackground(new Color(193,66,66)); //  set bg color
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // closing behavior
+        this.pack(); // sizes screen properly
+        this.setVisible(true);
+        this.setLocationRelativeTo(null); // game will appear on middle of screen
+    }
+}
